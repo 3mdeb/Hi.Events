b7ab8f49155f6971e16563bc50864ec72216d4b7
Artur Raglis <artur.raglis@3mdeb.com>
2025-06-02 20:22:02 +0200

    frontend/src/components/layouts/EventHomepage/PretalxScheduleWidget.tsx: remove console logs
    Signed-off-by: Artur Raglis <artur.raglis@3mdeb.com>

--- frontend/src/components/layouts/EventHomepage/PretalxScheduleWidget.tsx
+++ frontend/src/components/layouts/EventHomepage/PretalxScheduleWidget.tsx
@@ -36,19 +36,14 @@ export default function PretalxScheduleWidget(props: Props) {
   // Destructure known props, keep the rest for spreading
   const { eventUrl, locale, format, style: styleStringFromProps, ...restAttribs } = props;
 
-  console.log("[PretalxScheduleWidget] mounted with eventUrl:", eventUrl);
-
   useEffect(() => {
     const scriptSrc = `${eventUrl.replace(/\/$/, '')}/widgets/schedule.js`;
-    console.log("[PretalxScheduleWidget] injecting script:", scriptSrc);
 
     if (!document.querySelector(`script[src="${scriptSrc}"]`)) {
       const script = document.createElement('script');
       script.src = scriptSrc;
       script.async = true;
       document.head.appendChild(script);
-    } else {
-      console.log("[PretalxScheduleWidget] script already injected");
     }
   }, [eventUrl]);
 
